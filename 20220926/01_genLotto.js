f4c805fbdbcf335a1225547b35199ae595ae2006
letter333 <seryong12@gmail.com>
2022-09-26 12:40:25 +0900

    js practice

---
+++ 20220926/01_genLotto.js
@@ -0,0 +1,30 @@
+function genLotto() {
+    let lotto = []
+
+    for(let i = 0; i < 7; i++) {
+        let num = Math.floor(Math.random() * 45) + 1
+        let isDuplicate = false
+        for(let j = 0; j < i; j++) {
+            if(num == lotto[j]) {
+                isDuplicate = true
+                i--
+                break
+            }
+        }
+        if(isDuplicate == false) {
+            lotto.push(num)
+        }
+    }
+
+    for(let i = 0; i < lotto.length; i++) {
+        for(let j = 0; j < lotto.length - 1 - i; j++) {
+            if(lotto[j] > lotto[j + 1]) {
+                let temp = lotto[j]
+                lotto[j] = lotto[j + 1]
+                logtto[j + 1] = temp
+            }
+        }
+    }
+
+    return lotto
+}
